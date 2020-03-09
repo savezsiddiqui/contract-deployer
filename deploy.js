c5b388621e407305858a9a4ee906bdc1fd8abe77
savezsiddiqui <savezsiddiqui@gmail.com>
2020-03-10 00:32:12 +0530

    fixed deploy.js

---
+++ deploy.js
@@ -0,0 +1,31 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'bounce biology skirt tuna marriage tower treat echo usual hollow orient before',
+    'https://ropsten.infura.io/v3/be76cd96bbba4cb8bc7ef15a6af1db14'
+)
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+    let result;
+
+    try {
+        result = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({ data: '0x' + bytecode, arguments: ['Hi There!'] })
+            .send({ gas: '1000000', from: accounts[0] });
+    }
+    catch (err) {
+        console.log('Error: ', err.message);
+    }
+
+    if (result) {
+        console.log('Contract deployed to', result.options.address);
+    }
+}
+
+deploy();
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -6,56 +6,56 @@ const { interface, bytecode } = require('../compile');
 
 let accounts, inbox;
 
-// beforeEach(async () => {
-//     accounts = await web3.eth.getAccounts();
-//     inbox = await new web3.eth.Contract(JSON.parse(interface))
-//         .deploy({ data: bytecode, arguments: ['Hi There !'] })
-//         .send(({ from: accounts[0], gas: '1000000' }));
-// });
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi There !'] })
+        .send(({ from: accounts[0], gas: '1000000' }));
+});
 
-// describe('Inbox', () => {
+describe('Inbox', () => {
 
-//     it('deploys a contract', () => {
-//         assert.ok(inbox.options.address);
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi There !');
+    })
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye').send({ from: accounts[0] })
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'bye');
+    })
+});
+
+// describe('Lottery', () => {
+
+//     it('requires a minimum amount of ether', async () => {
+//         try {
+//             await lottery.methods.enter().send({
+//                 from: accounts[0],
+//                 value: 0
+//             });
+//             assert(false);
+//         }
+//         catch (err) {
+//             assert(err);
+//         }
 //     });
 
-//     it('has a default message', async () => {
-//         const message = await inbox.methods.message().call();
-//         assert.equal(message, 'Hi There !');
-//     })
+//     it('Only manager can pick the winner', async () => {
+//         try {
+//             await lottery.methods.pickWinner().send({
+//                 from: accounts[1]
+//             });
+//             assert(false);
+//         }
+//         catch (err) {
+//             assert(err)
+//         }
+//     });
 
-//     it('can change the message', async () => {
-//         await inbox.methods.setMessage('bye').send({ from: accounts[0] })
-//         const message = await inbox.methods.message().call();
-//         assert.equal(message, 'bye');
-//     })
 // });
-
-describe('Lottery', () => {
-
-    it('requires a minimum amount of ether', async () => {
-        try {
-            await lottery.methods.enter().send({
-                from: accounts[0],
-                value: 0
-            });
-            assert(false);
-        }
-        catch (err) {
-            assert(err);
-        }
-    });
-
-    it('Only manager can pick the winner', async () => {
-        try {
-            await lottery.methods.pickWinner().send({
-                from: accounts[1]
-            });
-            assert(false);
-        }
-        catch (err) {
-            assert(err)
-        }
-    });
-
-});
